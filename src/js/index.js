953df11eab80c6733300d850570ee23f8e1b4882
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2021-05-03 09:17:06 +0800

    add screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 var quickFab = new mdui.Fab('#quickFab');
+var phoneScreen = document.querySelector('#phone-screen');
 
 var openSettings = document.querySelectorAll("#open-settings");
 openSettings.forEach((e) => {
     e.addEventListener("click", () => {
         document.querySelector('.settings-hover-layer').classList.add('show');
         document.querySelector('.settings-wrapper').classList.add('show');
+        document.querySelector('.settings-wrapper').classList.remove('hide');
         document.querySelector('body').style.overflow = 'hidden';
     })
 })
@@ -13,7 +15,20 @@ var closeSettings = document.querySelectorAll("#close-settings");
 closeSettings.forEach((e) => {
     e.addEventListener("click", () => {
         document.querySelector('.settings-hover-layer').classList.remove('show');
-        document.querySelector('.settings-wrapper').classList.remove('show')
+        document.querySelector('.settings-wrapper').classList.remove('show');
+        document.querySelector('.settings-wrapper').classList.add('hide');
         document.querySelector('body').style.overflow = 'scroll';
     })
+
 })
+document.querySelector('.settings-hover-layer').addEventListener("click", () => {
+    document.querySelector('.settings-hover-layer').classList.remove('show');
+    document.querySelector('.settings-wrapper').classList.remove('show')
+    document.querySelector('body').style.overflow = 'scroll';
+})
+
+function power(isOn) {
+    if (isOn) {
+        phoneScreen.setAttribute('src', 'screen/on.html');
+    }
+}
